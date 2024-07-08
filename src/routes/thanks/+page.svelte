9570d100eeaed1b9c201909d35fd6fc99b16ce5f
Lucas Maunu <evolved.cow@gmail.com>
2024-07-08 14:42:49 -0500

    Fix the names

--- src/routes/thanks/+page.svelte
+++ src/routes/thanks/+page.svelte
@@ -2,8 +2,8 @@
 <p>FreedomHistory.net wouldn't have been possible without:</p>
 <ol>
     <li><a href="https://freesound.org/people/mpaol2023/sounds/370178/">Freesound</a></li>
-    <li><a href="https://kit.svelte.dev/">svelte kit</a></li>
+    <li><a href="https://kit.svelte.dev/">SvelteKit</a></li>
     <li><a href="https://picocss.com/">Pico CSS</a></li>
-    <li><a href="https://github.com/Evolved-Cow-Man/freedom-history-net/">Git Hub</a></li>
+    <li><a href="https://github.com/Evolved-Cow-Man/freedom-history-net/">GitHub</a></li>
     <li><a href="https://www.cloudflare.com/">Cloudflare</a></li>
 </ol>
